e56fcac2132ff1a18420cccb6a8acd1e2aece642
Pirata <campero100820@gmail.com>
2023-07-09 18:59:14 -0600

    Add colors for terminal

--- WebScanruby.rb
+++ WebScanruby.rb
@@ -1,23 +1,46 @@
 #!/usr/bin/env ruby
 # Created By Pirata
 #
+#
 
 require 'wappalyzer'
 
-analizer=Wappalyzer::Detector.new
-
-results=analizer.analyze('http://192.168.225.245')
-
-puts results
-
-results.each do |tech|
-  puts tech[0]
-  puts "Categories"
-  tech[1]['categories'].each do |cat|
-    puts "-> #{cat}"
-  end
-  puts "Confidence #{tech[1]['confidence']}"
-  puts "Version #{tech[1]['version']}"
-
+class String
+  def red; colorize(self, "\e[1m\e[31m"); end
+  def green; colorize(self, "\e[1m\e[32m"); end
+  def dark_green; colorize(self, "\e[32m"); end
+  def yellow; colorize(self, "\e[1m\e[33m"); end
+  def blue; colorize(self, "\e[1m\e[34m"); end
+  def dark_blue; colorize(self, "\e[34m"); end
+  def purple; colorize(self, "\e[35m"); end
+  def dark_purple; colorize(self, "\e[1;35m"); end
+  def cyan; colorize(self, "\e[1;36m"); end
+  def dark_cyan; colorize(self, "\e[36m"); end
+  def pure; colorize(self, "\e[0m\e[28m"); end
+  def bold; colorize(self, "\e[1m"); end
+  def colorize(text, color_code) "#{color_code}#{text}\e[0m" end
 end
+
+
+def detect_tech(url)
+  analizer=Wappalyzer::Detector.new
+  results=analizer.analyze(url)
+
+  puts "*".red*50
+  results.each do |tech|
+    puts "Technology:".purple+" #{tech[0]}".blue
+    puts "Categories: ".purple
+    
+    tech[1]['categories'].each do |cat|
+      puts "\t ->".yellow+" #{cat}"
+    end
+    
+    puts "Confidence: ".purple+"#{tech[1]['confidence']}"
+    puts "Version: ".purple+"#{tech[1]['version']}\n\n"
+  end
+
+  puts "*".red*50
+end
+
+detect_tech('http://192.168.225.245')
 
